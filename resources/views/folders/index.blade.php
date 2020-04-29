ca44ecb946b5f389290be27334d86de10741f18f
K-GH <khalled.ghonaim95@gmail.com>
2020-04-29 23:07:47 +0200

    'create

--- resources/views/folders/index.blade.php
+++ resources/views/folders/index.blade.php
@@ -9,6 +9,7 @@
       <div class="card border-primary mb-3" style="max-width: 20rem;">
           <div class="card-header"> <a href="/folders/{{$folder->id}}"> {{$folder->name}} </a> </div>
           <div class="card-body">
+             <h4 class="card-title">Description</h4>
             <p class="card-text">{{$folder->description}}.</p>
           </div>
       </div>
