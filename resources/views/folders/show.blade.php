17b95b0a6a47668b2a16dac674a2ce6ed8091c7f
K-GH <khalled.ghonaim95@gmail.com>
2020-04-30 22:37:31 +0200

    'showFiles'

--- resources/views/folders/show.blade.php
+++ resources/views/folders/show.blade.php
@@ -3,7 +3,20 @@
   <h2>Files of {{$folder->name}}</h2>
   <a href="/files/create/{{$folder->id}}" class="btn btn-primary " >Upload files</a>
   @if(count($folder->files)>0)
-        <h2>Files loaded</h2>
+      <h2>Files loaded</h2>
+
+        <div class="row">
+            @foreach($folder->files as $file)
+
+                <div class="col-sm">
+                    <div class="alert alert-dismissible alert-primary">
+                          <h4>{{$file->title}}</h4>
+                          <p>{{$file->description}}</p>
+                          <a href="/files/{{$file->id}}"> Click to show</a>
+                    </div>
+                </div>
+            @endforeach
+      </div>
   @else
       <br><br>
       <div class="badge badge-danger">
