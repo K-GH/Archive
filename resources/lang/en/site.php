29f01c43688bc2648c161915c6f493c6db3d5431
K-GH <khalled.ghonaim95@gmail.com>
2020-07-01 22:49:25 +0200

    English site.php setup

---
+++ resources/lang/en/site.php
@@ -0,0 +1,46 @@
+<?php
+return [
+'archive'=>'Archive',
+'home'=>'Home',
+'about'=>'About',
+'contact'=>'Contact Us',
+'login'=>'Login',
+'register'=>'Register',
+'Logout'=>'Logout',
+'upload_files'=>'Upload Files ',
+'dashboard'=>'Dashboard ',
+'languages'=>'Languages',
+'learn_more'=>'Learn More',
+
+'name'=>'Name',
+'username'=>'Username ',
+'password'=>'Password ',
+'confirm_password'=>'Confirm Password  ',
+'e-mail_address'=>'E-mail Address ',
+
+
+'remember_me'=>'Remember Me',
+'forgot_your_password'=>'Forgot Your Password   ',
+
+'welcome_back'=>'Welcome Back ',
+'you_are_logged_in'=>'You are logged In',
+'last_login_at'=>'Last login At',
+'last_login_ip'=>'Last login IP',
+
+
+'search_topics_or_keywords'=>'Search Topics Or Keywords',
+
+'your_folders'=>'Your Folders',
+'create_folder'=>'Create a Folder',
+'folder_name'=>'Folder Name',
+'description'=>'Description',
+'file_title'=>'File Title',
+'create_file'=>'Add a File',
+
+'create'=>'Create',
+'add'=>'Add',
+
+
+'Back'=>'Back previous page',
+
+];
